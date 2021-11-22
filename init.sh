42191de895c626388b768cc527b41418dea012df
flysoar <f1ys0ar7@gmail.com>
2021-11-22 15:40:55 +0800

    t

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 git init
 git config credential.helper store
 git config user.name flysoar
-git config user.password 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
+git config user.password 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
 git add *
 git commit -m "t"
 git branch -M main
